9bccf697adf7d75be73e659c8032db05a9ec5347
AntonioRogelUK <63703411+AntonioRogelUK@users.noreply.github.com>
2021-05-22 14:43:35 -0700

    Agregar archivos de proyecto.

---
+++ ArbolesGenerales/Nodo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolesGenerales
+{
+    public class Nodo
+    {
+        private string dato;
+        private Nodo hijo;
+        private Nodo hermano;
+
+        public string Dato { get => dato; set => dato = value; }
+        public Nodo Hijo { get => hijo; set => hijo = value; }
+        public Nodo Hermano { get => hermano; set => hermano = value; }
+    }
+}
---
+++ ED2_052021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ED2_052021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
