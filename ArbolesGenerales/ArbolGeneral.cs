5d92877f28d9d26ef92647dd7a65ac81a043a70d
AntonioRogelUK <63703411+AntonioRogelUK@users.noreply.github.com>
2021-06-02 21:37:45 -0700

    Insertar nodos

--- ArbolesGenerales/ArbolGeneral.cs
+++ ArbolesGenerales/ArbolGeneral.cs
@@ -15,5 +15,36 @@ namespace ArbolesGenerales
         {
             raiz = new Nodo(dato);
         }
+
+        public Nodo InsertarNodo(string dato, Nodo nodoPadre) 
+        {
+            if (string.IsNullOrWhiteSpace(dato)) 
+            {
+                throw new Exception("No se puede insertar un valor vacío en dato");
+            }
+
+            if (nodoPadre == null) 
+            {
+                throw new Exception("No se puede insertar un nodo, sin especificar el padre");
+            }
+
+            if (nodoPadre.Hijo == null)
+            {
+                nodoPadre.Hijo = new Nodo(dato);
+                return nodoPadre.Hijo;
+            }
+            else 
+            {
+                Nodo hijoActual = nodoPadre.Hijo;
+
+                while (hijoActual.Hermano != null) 
+                {
+                    hijoActual = hijoActual.Hermano;
+                }
+
+                hijoActual.Hermano = new Nodo(dato);
+                return hijoActual.Hermano;
+            }
+        }
     }
 }
--- ArbolesGenerales/Program.cs
+++ ArbolesGenerales/Program.cs
@@ -7,7 +7,23 @@ namespace ArbolesGenerales
         static void Main(string[] args)
         {
             ArbolGeneral arbol = new ArbolGeneral("A");
-            
+
+            Nodo nodoB = arbol.InsertarNodo("B", arbol.Raiz);
+            Nodo nodoC = arbol.InsertarNodo("C", arbol.Raiz);
+
+            Nodo nodoD = arbol.InsertarNodo("D", nodoB);
+            Nodo nodoE = arbol.InsertarNodo("E", nodoB);
+            Nodo nodoF = arbol.InsertarNodo("F", nodoB);
+
+            Nodo nodoI = arbol.InsertarNodo("I", nodoD);
+
+            Nodo nodoJ = arbol.InsertarNodo("J", nodoF);
+            Nodo nodoK = arbol.InsertarNodo("K", nodoF);
+
+            Nodo nodoG = arbol.InsertarNodo("G", nodoC);
+            Nodo nodoH = arbol.InsertarNodo("H", nodoC);
+
+            Nodo nodoL = arbol.InsertarNodo("L", nodoH);
 
             Console.WriteLine(arbol.Raiz.Dato);
         }
