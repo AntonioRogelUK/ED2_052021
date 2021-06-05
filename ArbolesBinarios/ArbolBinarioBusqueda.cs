3fccb2fb4aa7969961e80362b94e690e4bc04bfd
AntonioRogelUK <63703411+AntonioRogelUK@users.noreply.github.com>
2021-06-05 14:47:03 -0700

    Arbol binario de busqueda

---
+++ ArbolesBinarios/ArbolBinarioBusqueda.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolesBinarios
+{
+    public class ArbolBinarioBusqueda
+    {
+        private readonly Nodo raiz;
+        public Nodo Raiz { get { return raiz; } }
+        public ArbolBinarioBusqueda(int dato)
+        {
+            this.raiz = new Nodo(dato);
+        }
+        
+        public enum TipoRecorrido 
+        {
+            Preorden,
+            Inorden,
+            Posorden
+        }
+
+        public void Insertar(int dato, Nodo nodo = null)
+        {
+            if (nodo == null) 
+            {
+                nodo = this.raiz;
+            }
+
+            if (dato > nodo.Dato) 
+            {
+                if (nodo.SubArbolDerecho == null) 
+                {
+                    nodo.SubArbolDerecho = new Nodo(dato);
+                }
+
+                Insertar(dato, nodo.SubArbolDerecho);
+            }
+            else if (dato < nodo.Dato) 
+            {
+                if (nodo.SubArbolIzquierdo == null) 
+                {
+                    nodo.SubArbolIzquierdo = new Nodo(dato);
+                }
+
+                Insertar(dato, nodo.SubArbolIzquierdo);
+            }
+        }
+    }
+}
---
+++ ArbolesBinarios/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ArbolesBinarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArbolBinarioBusqueda abb = new ArbolBinarioBusqueda(5);
+
+            
+
+        }
+    }
+}
